b38c31ccdf39c44859289c5258309167b89c49eb
Dmitriy Lopukhov <dmitriy@admins-MacBook-Pro.local>
2019-09-28 17:04:41 +0200

    geolocation plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+    Geolocation,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
