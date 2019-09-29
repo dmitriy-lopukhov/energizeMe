24da4ae8af71a6f0d79ea95a44a70581bad0d65f
Dmitriy Lopukhov <dmitriy@admins-MacBook-Pro.local>
2019-09-29 12:31:44 +0200

    small fixes

--- src/app/core/constants/stats.ts
+++ src/app/core/constants/stats.ts
@@ -1,194 +1,194 @@
 export const stats = [
     {
-        'Price 1': '0.36',
-        'Price 2': '0.35',
-        'Price 3': '0.33',
-        'Price 4': '0.33',
-        'Price 5': '0.33',
-        'Price 6': '0.36'
+        'Station 1': '0.36',
+        'Station 2': '0.35',
+        'Station 3': '0.33',
+        'Station 4': '0.33',
+        'Station 5': '0.33',
+        'Station 6': '0.36'
     },
     {
-        'Price 1': '0.37',
-        'Price 2': '0.41',
-        'Price 3': '0.41',
-        'Price 4': '0.39',
-        'Price 5': '0.43',
-        'Price 6': '0.35'
+        'Station 1': '0.37',
+        'Station 2': '0.41',
+        'Station 3': '0.41',
+        'Station 4': '0.39',
+        'Station 5': '0.43',
+        'Station 6': '0.35'
     },
     {
-        'Price 1': '0.50',
-        'Price 2': '0.48',
-        'Price 3': '0.44',
-        'Price 4': '0.47',
-        'Price 5': '0.47',
-        'Price 6': '0.47'
+        'Station 1': '0.50',
+        'Station 2': '0.48',
+        'Station 3': '0.44',
+        'Station 4': '0.47',
+        'Station 5': '0.47',
+        'Station 6': '0.47'
     },
     {
-        'Price 1': '0.51',
-        'Price 2': '0.54',
-        'Price 3': '0.54',
-        'Price 4': '0.53',
-        'Price 5': '0.54',
-        'Price 6': '0.51'
+        'Station 1': '0.51',
+        'Station 2': '0.54',
+        'Station 3': '0.54',
+        'Station 4': '0.53',
+        'Station 5': '0.54',
+        'Station 6': '0.51'
     },
     {
-        'Price 1': '0.47',
-        'Price 2': '0.52',
-        'Price 3': '0.45',
-        'Price 4': '0.47',
-        'Price 5': '0.46',
-        'Price 6': '0.53'
+        'Station 1': '0.47',
+        'Station 2': '0.52',
+        'Station 3': '0.45',
+        'Station 4': '0.47',
+        'Station 5': '0.46',
+        'Station 6': '0.53'
     },
     {
-        'Price 1': '0.55',
-        'Price 2': '0.52',
-        'Price 3': '0.51',
-        'Price 4': '0.54',
-        'Price 5': '0.46',
-        'Price 6': '0.52'
+        'Station 1': '0.55',
+        'Station 2': '0.52',
+        'Station 3': '0.51',
+        'Station 4': '0.54',
+        'Station 5': '0.46',
+        'Station 6': '0.52'
     },
     {
-        'Price 1': '0.30',
-        'Price 2': '0.34',
-        'Price 3': '0.36',
-        'Price 4': '0.37',
-        'Price 5': '0.34',
-        'Price 6': '0.39'
+        'Station 1': '0.30',
+        'Station 2': '0.34',
+        'Station 3': '0.36',
+        'Station 4': '0.37',
+        'Station 5': '0.34',
+        'Station 6': '0.39'
     },
     {
-        'Price 1': '0.17',
-        'Price 2': '0.13',
-        'Price 3': '0.15',
-        'Price 4': '0.11',
-        'Price 5': '0.16',
-        'Price 6': '0.20'
+        'Station 1': '0.17',
+        'Station 2': '0.13',
+        'Station 3': '0.15',
+        'Station 4': '0.11',
+        'Station 5': '0.16',
+        'Station 6': '0.20'
     },
     {
-        'Price 1': '0.19',
-        'Price 2': '0.14',
-        'Price 3': '0.20',
-        'Price 4': '0.13',
-        'Price 5': '0.18',
-        'Price 6': '0.16'
+        'Station 1': '0.19',
+        'Station 2': '0.14',
+        'Station 3': '0.20',
+        'Station 4': '0.13',
+        'Station 5': '0.18',
+        'Station 6': '0.16'
     },
     {
-        'Price 1': '0.10',
-        'Price 2': '0.11',
-        'Price 3': '0.15',
-        'Price 4': '0.08',
-        'Price 5': '0.08',
-        'Price 6': '0.12'
+        'Station 1': '0.10',
+        'Station 2': '0.11',
+        'Station 3': '0.15',
+        'Station 4': '0.08',
+        'Station 5': '0.08',
+        'Station 6': '0.12'
     },
     {
-        'Price 1': '0.07',
-        'Price 2': '0.03',
-        'Price 3': '0.04',
-        'Price 4': '0.12',
-        'Price 5': '0.11',
-        'Price 6': '0.09'
+        'Station 1': '0.07',
+        'Station 2': '0.03',
+        'Station 3': '0.04',
+        'Station 4': '0.12',
+        'Station 5': '0.11',
+        'Station 6': '0.09'
     },
     {
-        'Price 1': '0.10',
-        'Price 2': '0.03',
-        'Price 3': '0.06',
-        'Price 4': '0.06',
-        'Price 5': '0.00',
-        'Price 6': '0.04'
+        'Station 1': '0.10',
+        'Station 2': '0.03',
+        'Station 3': '0.06',
+        'Station 4': '0.06',
+        'Station 5': '0.00',
+        'Station 6': '0.04'
     },
     {
-        'Price 1': '0.07',
-        'Price 2': '0.03',
-        'Price 3': '0.03',
-        'Price 4': '0.03',
-        'Price 5': '0.04',
-        'Price 6': '0.08'
+        'Station 1': '0.07',
+        'Station 2': '0.03',
+        'Station 3': '0.03',
+        'Station 4': '0.03',
+        'Station 5': '0.04',
+        'Station 6': '0.08'
     },
     {
-        'Price 1': '0.11',
-        'Price 2': '0.18',
-        'Price 3': '0.12',
-        'Price 4': '0.10',
-        'Price 5': '0.17',
-        'Price 6': '0.12'
+        'Station 1': '0.11',
+        'Station 2': '0.18',
+        'Station 3': '0.12',
+        'Station 4': '0.10',
+        'Station 5': '0.17',
+        'Station 6': '0.12'
     },
     {
-        'Price 1': '0.20',
-        'Price 2': '0.17',
-        'Price 3': '0.14',
-        'Price 4': '0.14',
-        'Price 5': '0.19',
-        'Price 6': '0.11'
+        'Station 1': '0.20',
+        'Station 2': '0.17',
+        'Station 3': '0.14',
+        'Station 4': '0.14',
+        'Station 5': '0.19',
+        'Station 6': '0.11'
     },
     {
-        'Price 1': '0.14',
-        'Price 2': '0.14',
-        'Price 3': '0.09',
-        'Price 4': '0.09',
-        'Price 5': '0.14',
-        'Price 6': '0.07'
+        'Station 1': '0.14',
+        'Station 2': '0.14',
+        'Station 3': '0.09',
+        'Station 4': '0.09',
+        'Station 5': '0.14',
+        'Station 6': '0.07'
     },
     {
-        'Price 1': '0.21',
-        'Price 2': '0.24',
-        'Price 3': '0.25',
-        'Price 4': '0.24',
-        'Price 5': '0.30',
-        'Price 6': '0.29'
+        'Station 1': '0.21',
+        'Station 2': '0.24',
+        'Station 3': '0.25',
+        'Station 4': '0.24',
+        'Station 5': '0.30',
+        'Station 6': '0.29'
     },
     {
-        'Price 1': '0.22',
-        'Price 2': '0.30',
-        'Price 3': '0.28',
-        'Price 4': '0.26',
-        'Price 5': '0.30',
-        'Price 6': '0.29'
+        'Station 1': '0.22',
+        'Station 2': '0.30',
+        'Station 3': '0.28',
+        'Station 4': '0.26',
+        'Station 5': '0.30',
+        'Station 6': '0.29'
     },
     {
-        'Price 1': '0.15',
-        'Price 2': '0.20',
-        'Price 3': '0.19',
-        'Price 4': '0.16',
-        'Price 5': '0.20',
-        'Price 6': '0.21'
+        'Station 1': '0.15',
+        'Station 2': '0.20',
+        'Station 3': '0.19',
+        'Station 4': '0.16',
+        'Station 5': '0.20',
+        'Station 6': '0.21'
     },
     {
-        'Price 1': '0.20',
-        'Price 2': '0.24',
-        'Price 3': '0.17',
-        'Price 4': '0.21',
-        'Price 5': '0.18',
-        'Price 6': '0.24'
+        'Station 1': '0.20',
+        'Station 2': '0.24',
+        'Station 3': '0.17',
+        'Station 4': '0.21',
+        'Station 5': '0.18',
+        'Station 6': '0.24'
     },
     {
-        'Price 1': '0.24',
-        'Price 2': '0.23',
-        'Price 3': '0.26',
-        'Price 4': '0.26',
-        'Price 5': '0.28',
-        'Price 6': '0.22'
+        'Station 1': '0.24',
+        'Station 2': '0.23',
+        'Station 3': '0.26',
+        'Station 4': '0.26',
+        'Station 5': '0.28',
+        'Station 6': '0.22'
     },
     {
-        'Price 1': '0.28',
-        'Price 2': '0.26',
-        'Price 3': '0.25',
-        'Price 4': '0.29',
-        'Price 5': '0.24',
-        'Price 6': '0.23'
+        'Station 1': '0.28',
+        'Station 2': '0.26',
+        'Station 3': '0.25',
+        'Station 4': '0.29',
+        'Station 5': '0.24',
+        'Station 6': '0.23'
     },
     {
-        'Price 1': '0.31',
-        'Price 2': '0.25',
-        'Price 3': '0.22',
-        'Price 4': '0.29',
-        'Price 5': '0.26',
-        'Price 6': '0.30'
+        'Station 1': '0.31',
+        'Station 2': '0.25',
+        'Station 3': '0.22',
+        'Station 4': '0.29',
+        'Station 5': '0.26',
+        'Station 6': '0.30'
     },
     {
-        'Price 1': '0.27',
-        'Price 2': '0.27',
-        'Price 3': '0.31',
-        'Price 4': '0.31',
-        'Price 5': '0.25',
-        'Price 6': '0.34'
+        'Station 1': '0.27',
+        'Station 2': '0.27',
+        'Station 3': '0.31',
+        'Station 4': '0.31',
+        'Station 5': '0.25',
+        'Station 6': '0.34'
     }
 ];
